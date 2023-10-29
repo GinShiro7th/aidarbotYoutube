35b754f03d549f740538e74a2154a329c7272661
Evgeniy <50876285+evgrg@users.noreply.github.com>
2023-10-29 23:02:07 +0300

    Update writeComment.js

--- writeComment.js
+++ writeComment.js
@@ -5,8 +5,8 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
 puppeteer.use(StealthPlugin());
 
-
-module.exports = async function(url) {
+//123
+module.exports = async function(url){
 
   const browser = await puppeteer.launch({
     headless: false,
@@ -18,39 +18,36 @@ module.exports = async function(url) {
     ],
   });
 
-  const end = cookies.length - 1;
-  const curr = 0;
+  for (const acc of cookies){
+    const page = await browser.newPage();
 
-  const page = await browser.newPage();
-  await page.goto(url, {waitUntil: 'load'});
+    await page.setCookie(...acc.cookies);
+    await page.goto(url, { waitUntil: 'load' });
 
-  await page.evaluate(async () => {
-    window.scrollBy(0, 400);
-  });
+    await page.evaluate(async () => {
+      window.scrollBy(0, 400);
+    });
 
-  await page.waitForTimeout(200);
+    await page.waitForTimeout(100);
 
-  const frames = await page.frames()
-  const chatframe = frames.find(frame => frame.name() === 'chatframe');
-  let i = 0;
-  let interv = setInterval(async () => {
-    //for (const cock of cookies) {
-    let cock = cookies[i];
-    await page.setCookie(...cock.cookies);
-    console.log(curr);
+    const frames = await page.frames()
+    const chatframe = frames.find(frame => frame.name() === 'chatframe');
     try {
       const comment = await chatframe.$('#input.style-scope.yt-live-chat-message-input-renderer');
-      if (comment) {
+
+      if (comment){
         await comment.click();
-        await comment.type('hello' + (curr++));
+
+        await comment.type('hello');
+
         await page.keyboard.press('Enter');
       }
     } catch (err) {
       console.log('error writing comment:', err);
     }
-    //}
-    i++;
-    if (i > cookies.length) return clearInterval(interv);
-  }, 1000);
-  //await browser.close();
+
+    await page.waitForTimeout(3000);
+    break;
+  }
+  await browser.close();
 }
