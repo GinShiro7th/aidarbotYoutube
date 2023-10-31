426098a2f2824a949facf1629fa600b79e2248d9
Evgeniy <50876285+evgrg@users.noreply.github.com>
2023-10-31 23:46:14 +0300

    Update writeComment.js

--- writeComment.js
+++ writeComment.js
@@ -5,49 +5,88 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
 puppeteer.use(StealthPlugin());
 
-//123
-module.exports = async function(url){
+module.exports = async function (url) {
+  // for (const acc of cookies){
+  //   const browser = await puppeteer.launch({
+  //   headless: false,
+  //   args: [
+  //       "--no-sandbox",
+  //       "--disable-gpu",
+  //       "--enable-webgl",
+  //       "--window-size=1900,1200",
+  //     ],
+  //   });
+  //   const page = await browser.newPage();
 
-  const browser = await puppeteer.launch({
-    //headless: false,
-    args: [
-      "--no-sandbox",
-      "--disable-gpu",
-      "--enable-webgl",
-      "--window-size=1900,1270",
-    ],
-  });
+  //   await page.setCookie(...acc.cookies);
+  //   await page.goto(url);
 
-  for (const acc of cookies){
-    const page = await browser.newPage();
+  //   await page.waitForSelector('#chatframe');
+  //   const frames = await page.frames()
+  //   const chatframe = frames.find(frame => frame.name() === 'chatframe');
+  //   try {
+  //     const comment = await chatframe.$('#input.style-scope.yt-live-chat-message-input-renderer');
 
-    await page.setCookie(...acc.cookies);
-    await page.goto(url, { waitUntil: 'load' });
+  //     if (comment){
+  //       await comment.click();
 
-    /*await page.evaluate(async () => {
-      window.scrollBy(0, 400);
-    });*/
+  //       await comment.type('hello');
 
-    //await page.waitForTimeout(100);
+  //       await page.keyboard.press('Enter');
+  //     }
+  //   } catch (err) {
+  //     console.log('error writing comment:', err);
+  //   }
+  //   await browser.close();
+  //   //await page.close();
+  // }
 
-    const frames = await page.frames();
-    const chatframe = frames.find(frame => frame.name() === 'chatframe');
-    try {
-      await chatframe.waitForSelector('#input.style-scope.yt-live-chat-message-input-renderer');
-      const comment = await chatframe.$('#input.style-scope.yt-live-chat-message-input-renderer');
+  const end = cookies.length;
+  let curr = -1;
 
-      if (comment){
-        await comment.click();
+  const iter = setInterval(async () => {
+    if (++curr < end) {
+      console.log(curr);
+      const cock = cookies[curr].cookies;
 
-        await comment.type('закинь на деп');
+      const browser = await puppeteer.launch({
+        headless: false,
+        args: [
+          "--no-sandbox",
+          "--disable-gpu",
+          "--enable-webgl",
+          "--window-size=1900,1200",
+        ],
+      });
+      const page = await browser.newPage();
+      await page.setCookie(...cock);
 
-        await page.keyboard.press('Enter');
+      await page.goto(url);
+
+      await page.waitForSelector("#chatframe");
+      const frames = await page.frames();
+
+      const chatframe = frames.find((frame) => frame.name() === "chatframe");
+      try {
+        const comment = await chatframe.$(
+          "#input.style-scope.yt-live-chat-message-input-renderer"
+        );
+
+        if (comment) {
+          await comment.click();
+
+          await comment.type("hello");
+
+          await page.keyboard.press("Enter");
+        }
+      } catch (err) {
+        console.log("error writing comment:", err);
       }
-    } catch (err) {
-      console.log('error writing comment:', err);
+      await browser.close();
+    } else {
+      console.log("done");
+      clearInterval(iter);
     }
-
-    //await page.waitForTimeout(3000);
-  }
-  await browser.close();
-}
+  }, 2000);
+  // setTimeout(async () => await browser.close(), 5 * 60 * 1000);
+};
