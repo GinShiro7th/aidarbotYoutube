68b8b960cad60a9b19846a8dfe0c9c35c2d4bb54
Evgeniy <50876285+evgrg@users.noreply.github.com>
2023-11-05 15:25:32 +0300

    123

--- index.js
+++ index.js
@@ -13,7 +13,17 @@ const accounts = [
   {login : "morozzoovakatya@gmail.com", password: "Ct7gxFt8io"},
   {login : "elenazarkowa87@gmail.com", password: "CtufZgyjji9o"},
   {login : "sobcakk06@gmail.com", password: "Frygxtuh5789"},
-  {login : "olgabatutina849@gmail.com", password: "CtyhxCyoi578"}
+  {login : "olgabatutina849@gmail.com", password: "CtyhxCyoi578"},
+  {login : "ronaldherrans@gmail.com", password: "DSS@qer44t"},
+  {login : "fredhanerse@gmail.com", password: "DSS@box88i"},
+  {login : "donaldhudeers@gmail.com", password: "DSS@nox66y"},
+  {login : "teddylamores@gmail.com", password: "red55@CX55T"},
+  {login : "larymoters@gmail.com", password: "DSS@cer44"},
+  {login : "taylornorrads@gmail.com", password: "TYR@1200po"},
+  {login : "victorrobenson22@gmail.com", password: "DSS@axe44r"},
+  {login : "todorsdaniel78@gmail.com", password: "DSS@axe55t"},
+  {login : "pollersrobert@gmail.com", password: "DSS@qer44h"},
+  {login : "laneesharry99@gmail.com", password: "DSR55@qer33"},
 ];
 
 const url = 'https://www.youtube.com/watch?v=60-yvrNbom4';
@@ -28,5 +38,5 @@ const channel = 'https://www.youtube.com/@1337LikeR';
 
 
 // setTimeout(async () => await subscribe(channel), 1000);
-// setTimeout(async () => await writeComment(url), 1000);
+//setTimeout(async () => await writeComment(url), 1000);
 // setTimeout(async () => await loginAccount(login, password), 3000);
