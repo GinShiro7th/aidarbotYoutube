68ac6ba8f91cb3b82808bda0ee3153576e57e2dd
GinShiro7th <reretiv@gmail.com>
2023-11-06 21:15:12 +0300

    123

--- writeComment/main.js
+++ writeComment/main.js
@@ -1,7 +1,5 @@
 const { Worker, isMainThread } = require("worker_threads");
-
 const cookies = require("../cookies.json");
-
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
@@ -11,51 +9,29 @@ module.exports = async function (url) {
   if (isMainThread) {
     // Этот код выполняется в главном потоке
 
-    const pagesPerBrowser = 4;
-    const numBrowsers = cookies.length / pagesPerBrowser; // Здесь указываете, сколько браузеров нужно создать
+    const numBrowsers = 1; // Здесь указываете, сколько браузеров нужно создать
     const workers = [];
 
     for (let i = 0; i < numBrowsers; i++) {
-      // Создаем отдельный экземпляр браузера
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-          "--no-sandbox",
-          "--disable-gpu",
-          "--enable-webgl",
-          "--window-size=1900,1200",
-          '--disable-dev-shm-usage',
-        ],
+      const worker = new Worker("./writeComment/worker.js", {
+        workerData: {
+          cookies: cookies.slice(i * 5, (i + 1) * 5),
+          url,
+        },
       });
-      const endPoint = await browser.wsEndpoint();
 
-      // Создаем отдельные рабочие потоки для каждой страницы в браузере
-      for (let j = i * pagesPerBrowser; j < (i + 1) * pagesPerBrowser; j++) {
-        if (j < cookies.length) {
-          const worker = new Worker("./writeComment/worker.js", {
-            workerData: {
-              cookies: cookies[j],
-              url,
-              endPoint
-            },
-          });
-
-          worker.on("message", (message) => {
-            if (message.status === "done") {
-              console.log(
-                `Рабочий поток ${j} завершил работу в браузере ${i}.`
-              );
-            } else if (message.status === 'error'){
-              console.log(`ошибка в потоке ${j} в браузере ${i} - ${message.errMsg}`);
-            }
-          });
-
-          workers.push(worker);
+      worker.on("message", (message) => {
+        if (message.status === "done") {
+          console.log(`Рабочий поток браузера ${i} завершил работу.`);
+        } else if (message.status === 'error'){
+          console.log(`ошибка в рабочем потоке браузера ${i} - ${message.errMsg}`);
         }
-      }
+      });
+
+      workers.push(worker);
     }
     return;
   } else {
-    // Этот код не будет выполняться, так как worker.js используется в рабочих потоках
+    // Этот код не будет выполняться в главном потоке
   }
 };
--- writeComment/worker.js
+++ writeComment/worker.js
@@ -1,4 +1,3 @@
-// worker.js
 const { parentPort, workerData } = require('worker_threads');
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
@@ -6,44 +5,49 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
 
 (async () => {
-  const browser = await puppeteer.connect({ browserWSEndpoint: workerData.endPoint});
-  const acc = workerData.cookies;
-  
+  const cookies = workerData.cookies;
+  const url = workerData.url;
+
+  // Создаем отдельный экземпляр браузера
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      "--no-sandbox",
+      "--disable-gpu",
+      "--enable-webgl",
+      "--window-size=1900,1200",
+      '--disable-dev-shm-usage',
+    ],
+  });
   const context = await browser.createIncognitoBrowserContext();
-  const page = await context.newPage();
-  await page.setCookie(...acc.cookies);
-  
+
   try {
-    await page.goto(workerData.url, { timeout: 120 * 1000 });
-    
-    await page.waitForSelector("#chatframe", {timeout: 60 * 1000});
-    const frames = await page.frames();
-    const chatframe = frames.find((frame) => frame.name() === "chatframe");
-    
-    await chatframe.waitForSelector("#input.style-scope.yt-live-chat-message-input-renderer", {timeout: 60 * 1000});
-    
-    // await page.waitForTimeout(500);
+    for (const cookie of cookies) {
+      const page = await context.newPage();
+      await page.setCookie(...cookie.cookies);
 
-    const comment = await chatframe.$(
-      "#input.style-scope.yt-live-chat-message-input-renderer"
-    );
+      await page.goto(url, { timeout: 120 * 1000 });
+      await page.waitForSelector("#chatframe", { timeout: 60 * 1000 });
+      const frames = await page.frames();
+      const chatframe = frames.find((frame) => frame.name() === "chatframe");
+      await chatframe.waitForSelector("#input.style-scope.yt-live-chat-message-input-renderer", { timeout: 60 * 1000 });
 
-    if (comment) {
-      await comment.click();
-      await comment.type("hello");
-      await page.keyboard.press("Enter");
+      const comment = await chatframe.$("#input.style-scope.yt-live-chat-message-input-renderer");
+
+      if (comment) {
+        await comment.click();
+        await comment.type("hello");
+        await page.keyboard.press("Enter");
+      }
+
+      await page.waitForTimeout(500);
+      await context.close();
     }
-    
-    // await page.waitForTimeout(500);
-    // await browser.close();
 
-    
     parentPort.postMessage({ status: 'done' });
   } catch (err) {
-
-    await page.waitForTimeout(500);
     await browser.close();
-    
+
     parentPort.postMessage(
       { 
         status: 'error',
@@ -51,5 +55,4 @@ puppeteer.use(StealthPlugin());
       }
     );
   }
-
 })();
