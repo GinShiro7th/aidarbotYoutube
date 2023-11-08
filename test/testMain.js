666ef6acf3bdb5ce60c9ee91f8143150805ca655
GinShiro7th <reretiv@gmail.com>
2023-11-08 22:04:38 +0300

    123

--- test/testMain.js
+++ test/testMain.js
@@ -2,12 +2,11 @@ const { Cluster } = require('puppeteer-cluster');
 const cookies = require('../cookies.json');
 
 (async () => {
-  const numAccounts = cookies.length;
-  const maxConcurrency = 10; // Максимальное количество аккаунтов, обрабатываемых одновременно
-
-  const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: maxConcurrency,
+  const numBrowsers = 7; // Количество браузеров
+  const numPagesPerBrowser = 10; // Количество страниц в каждом браузере
+  const clusterOptions = {
+    concurrency: Cluster.CONCURRENCY_BROWSER, // Один браузер с несколькими страницами
+    maxConcurrency: numBrowsers, // Максимальное количество одновременно открытых браузеров
     puppeteerOptions: {
       headless: false,
       args: [
@@ -18,37 +17,49 @@ const cookies = require('../cookies.json');
         '--disable-dev-shm-usage',
       ],
     },
-  });
+  };
+  const cluster = await Cluster.launch(clusterOptions);
 
+  // Обработка каждой страницы
   await cluster.task(async ({ page, data }) => {
-    const acc = data.account;
-    await page.setCookie(...acc.cookies);
-    await page.goto(data.url, { timeout: 60000 });
+    try {
+      await page.setCookie(...cookies[data.cookieIndex].cookies);
+      await page.goto(data.url, { timeout: 120 * 1000 });
 
-    const frames = await page.frames();
-    const chatframe = frames.find((frame) => frame.name() === "chatframe");
+      await page.setCookie(...cookies[data.pageIndex].cookies);
 
-    if (chatframe) {
-      try {
-        const comment = await chatframe.$("#input.style-scope.yt-live-chat-message-input-renderer");
-        if (comment) {
-          const number = generateRandomNumberWithinRange(10, 100, 2);
-          await comment.click();
-          await comment.type(`${number}`);
-          await page.keyboard.press("Enter");
-          console.log(`Аккаунт ${acc.login} отправил комментарий.`);
-        }
-      } catch (err) {
-        console.error(`Ошибка аккаунта ${acc.login}:`, err);
+      await page.goto(url, { timeout: 120 * 1000 });
+      await page.waitForSelector("#chatframe", { timeout: 60 * 1000 });
+      const frames = await page.frames();
+      const chatframe = frames.find((frame) => frame.name() === "chatframe");
+      await chatframe.waitForSelector("#input.style-scope.yt-live-chat-message-input-renderer", { timeout: 60 * 1000 });
+
+      const comment = await chatframe.$("#input.style-scope.yt-live-chat-message-input-renderer");
+
+      if (comment) {
+        await comment.click();
+        await comment.type("hello");
+        await page.keyboard.press("Enter");
       }
+
+      console.log(`Страница ${data.pageIndex} в браузере ${data.browserIndex} открыта.`);
+      // Здесь вы можете выполнять дополнительные действия на странице
+    } catch (err) {
+      console.error(`Ошибка на странице ${data.pageIndex} в браузере ${data.browserIndex}:`, err);
     }
   });
 
-  for (let i = 0; i < numAccounts; i++) {
-    const taskData = { account: cookies[i], url: 'https://www.youtube.com/watch?v=60-yvrNbom4' };
-    cluster.queue(taskData);
+  // Генерация задач для каждой страницы в каждом браузере
+  for (let i = 0; i < numBrowsers; i++) {
+    for (let j = i * numPagesPerBrowser; j < (i + 1) * numPagesPerBrowser; j++) {
+      if (j < cookies.length){
+        const taskData = { url: 'https://www.youtube.com/watch?v=60-yvrNbom4', browserIndex: i, pageIndex: j, cookieIndex: i };
+        cluster.queue(taskData);
+      }
+    }
   }
 
+  // Дождитесь завершения всех задач
   await cluster.idle();
   await cluster.close();
 })();
