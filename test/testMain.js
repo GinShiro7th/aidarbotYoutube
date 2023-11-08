af3fadb6d85a18045548ca853858948b4ca0de08
GinShiro7th <reretiv@gmail.com>
2023-11-08 20:04:15 +0300

    123

--- test/testMain.js
+++ test/testMain.js
@@ -1,94 +1,54 @@
-const {
-  Worker,
-  isMainThread,
-  parentPort,
-  workerData,
-} = require("worker_threads");
+const { Cluster } = require('puppeteer-cluster');
+const cookies = require('../cookies.json');
 
-const cookies = require("../cookies.json");
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-
-puppeteer.use(StealthPlugin());
-
-module.exports = async function(url){
-  if (isMainThread) {
-    let browsersNum = cookies.length / 10;
-    const workers = [];
-
-    for (let i = 0; i < browsersNum; i++) {
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-          "--no-sandbox",
-          "--disable-gpu",
-          "--enable-webgl",
-          "--window-size=1900,1200",
-          "--disable-dev-shm-usage",
-        ],
-      });
-
-      const context = await browser.createIncognitoBrowserContext();
-
-      const workersNum = 10;
-      const pagesCount = {
-        num: 0,
-      };
-
-      for (let j = i * workersNum; j < workersNum * (i + 1); j++) {
-        if (j < cookies.length) {
-          const worker = new Worker(__filename, {
-            workerData: {
-              i,
-              j,
-            },
-          });
-
-          worker.on("message", async (msg) => {
-            const page = await context.newPage();
-            await page.setCookie(...cookies[j].cookies);
-            try {
-              await page.goto(url, { timeout: 120 * 1000 });
-              await page.waitForSelector("#chatframe", { timeout: 60 * 1000 });
-              const frames = await page.frames();
-              const chatframe = frames.find(
-                (frame) => frame.name() === "chatframe"
-              );
-              await chatframe.waitForSelector(
-                "#input.style-scope.yt-live-chat-message-input-renderer",
-                { timeout: 60 * 1000 }
-              );
-
-              const comment = await chatframe.$(
-                "#input.style-scope.yt-live-chat-message-input-renderer"
-              );
-
-              if (comment) {
-                await comment.click();
-                await comment.type("hello");
-                await page.keyboard.press("Enter");
-              }
-
-              await page.close();
-            } catch (err) {
-              await page.close();
-              console.log(`${cookies[j].login} - page error: `, err.message);
-            }
-            
-            console.log(`браузер ${msg.i} страница ${msg.j}`);
-            if (++pagesCount.num == workersNum) {
-              await browser.close();
-            }
-          });
-
-          workers.push(worker);
-        }
-      }
+(async () => {
+  // Создаем новый экземпляр кластера
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 2, // Максимальное количество одновременно открытых браузеров
+    puppeteerOptions: {
+      headless: false,
+      args: [
+        "--no-sandbox",
+        "--disable-gpu",
+        "--enable-webgl",
+        "--window-size=1900,1200",
+        '--disable-dev-shm-usage',
+      ],
     }
-  } else {
-    parentPort.postMessage({
-      i: workerData.i,
-      j: workerData.j,
-    });
-  }
-}
+  });
+
+  // Определяем задачу для обработки страницы
+  await cluster.task(async ({ page, data }) => {
+    await page.setCookie(...cookies[data.i].cookies);
+    try{
+      await page.goto(data.url, { timeout: 120 * 1000 });
+      console.log(123);
+      await page.waitForSelector("#chatframe", { timeout: 60 * 1000 });
+      const frames = await page.frames();
+      const chatframe = frames.find((frame) => frame.name() === "chatframe");
+      await chatframe.waitForSelector("#input.style-scope.yt-live-chat-message-input-renderer", { timeout: 60 * 1000 });
+
+      const comment = await chatframe.$("#input.style-scope.yt-live-chat-message-input-renderer");
+
+      if (comment) {
+        await comment.click();
+        await comment.type("hello");
+        await page.keyboard.press("Enter");
+      }
+    } catch (err) {
+      console.log('page err -', err.message);
+    }
+    await page.close();
+  });
+
+  // Добавляем задачи для обработки
+  for (let i = 0; i < 3; i++)
+    cluster.queue({ url: 'https://www.youtube.com/watch?v=60-yvrNbom4', i });
+
+  // Добавьте другие задачи...
+
+  // Ожидаем завершения всех задач
+  await cluster.idle();
+  await cluster.close();
+})();
