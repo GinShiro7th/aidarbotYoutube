3ac5c4773d395f63e86ee3f266a4a7b83ec8c033
GinShiro7th <reretiv@gmail.com>
2023-11-15 18:19:36 +0300

    Update stopCommenting.js

--- functions/bot/stopCommenting.js
+++ functions/bot/stopCommenting.js
@@ -4,4 +4,5 @@ const fs = require('fs');
 module.exports = async function(msg, bot){
   botState.stoped = true;
   fs.writeFile('./functions/botState.json', JSON.stringify(botState, null, 2), (err) => err ? console.log(err) : null);
+  await bot.sendMessage(msg.chat.id, "Написание комментариев остановлено");
 }
