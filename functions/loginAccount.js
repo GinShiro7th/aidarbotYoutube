c3c3cb9b953934c373ee2d91e2926bcd700a7cbd
GinShiro7th <reretiv@gmail.com>
2023-12-05 17:07:54 +0300

    Update loginAccount.js

--- functions/loginAccount.js
+++ functions/loginAccount.js
@@ -1,36 +1,84 @@
 const fs = require("fs");
+const solveImageCaptcha = require("./solveImageCaptcha.js");
 const cookies = require("../cookies.json");
 
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const recaptchaPlugin = require('puppeteer-extra-plugin-recaptcha');
+
+puppeteer.use(StealthPlugin());
+
+
 async function loginAccount(email, password, proxy) {
-  const { selfbot } = await import("youtube-selfbot-api");
 
-  let bot = new selfbot();
-  const browser = await bot.launch();
-  try{
+  const proxyServer = proxy.replace(/\/(.*?)@/g, "//");
+  const proxyUsername = proxy.substring(proxy.lastIndexOf('/')+1, proxy.indexOf('@')).split(':')[0];
+  const proxyPassword = proxy.substring(proxy.lastIndexOf('/')+1, proxy.indexOf('@')).split(':')[1];
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      "--no-sandbox",
+      "--disable-gpu",
+      "--enable-webgl",
+      "--window-size=800,800",
+      "--disable-web-security",
+      `--proxy-server=${proxyServer}`
+    ],
+  });
+
+  const loginUrl =
+    "https://accounts.google.com/AddSession?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den-GB%26next%3D%252F&hl=en-GB&passive=false&service=youtube&uilel=0";
+
+  try {
     const page = await browser.newPage();
-    const googleContext = await page.setupGoogle();
-    await googleContext.login({
-      email,
-      password,
-    });
+    const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0";
+    await page.setUserAgent(ua);
 
-    await page.goto("https://www.youtube.com");
+    await page.authenticate({username: proxyUsername, password: proxyPassword});
+    
+    await page.goto(loginUrl, { waitUntil: "networkidle2" });
+    
+    await page.type('input[type="email"]', email);
+    await page.keyboard.press("Enter");
+    await page.waitForTimeout(5000);
 
-    await page.waitForSelector(`#avatar-btn`, { timeout: 10000 });
+    try {
+      const captchaImgElement = await page
+        .$eval("#captchaimg", () => true)
+        .catch(() => false);
+      if (captchaImgElement) {
+        await solveImageCaptcha(page);
+      }
+    } catch (err) {
+      console.log("no captcha(");
+    }
+
+    await page.waitForTimeout(3000);
+
+    await page.type('input[type="password"]', password);
+    await page.keyboard.press("Enter");
+
+    await page.waitForNavigation();
+    const userCookies = await page.cookies();
+    await page.waitForSelector('#avatar-btn');
     await page.click("#avatar-btn");
-    await page.waitForSelector("#channel-container");
+    await page.waitForSelector('#channel-container');
     let nickname = await page.$eval(
       "#channel-container",
       (elem) => elem.textContent
     );
-    const userCookies = await page.getCookies();
-    await browser.close();
+    
+    console.log(nickname);
     const name = nickname.split("\n")[1].trim();
 
     console.log(name);
 
-    if (userCookies) {
-      console.log("saving cookies..");
+    await page.waitForTimeout(1000);
+
+    await browser.close();
+
+    if (userCookies[0].domain.includes("youtube")) {
       const index = cookies.findIndex(
         (item) => item.email === email && item.password === password
       );
@@ -52,9 +100,16 @@ async function loginAccount(email, password, proxy) {
       if (err) console.log(err);
     });
   } catch (err) {
-    console.log('error logging acc:', err.message);
+    console.log("email err", err);
     await browser.close();
   }
-}
+};
 
 module.exports = loginAccount;
+
+
+const email = "robinnopelles@gmail.com"
+const password = "DFY66y@ve4"
+const proxy = "http://YGP3OY:YCV4Cmz0ow@45.11.21.6:1050"
+
+loginAccount(email, password, proxy);
